760ae72c35cc0781e84af2defbe1e242fc594826
volgoweb <volgoweb@bk.ru>
2019-04-18 19:31:02 +0400

    Remove deprecated code

--- manage.py
+++ manage.py
@@ -13,13 +13,6 @@ def create_db():
     db.init_app(current_app)
     db.create_all()
 
-    # card = models.Card(title='dfd2f')
-    # add_action = models.Action(name='add')
-    # remove_action = models.Action(name='remove')
-    # card.actions.append(add_action)
-    # card.actions.append(remove_action)
-    # db.session.commit()
-
 
 if __name__ == "__main__":
     manager.run()
